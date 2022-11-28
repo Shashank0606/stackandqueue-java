b3771c6bb161cebddcc4ddfc791ffc7732751b9e
Shashank0606 <shashankrathore606@gmail.com>
2022-11-29 00:00:25 +0530

    REFACTOR: Peak and Pop From Stack

--- stackandqueue/src/main/java/com/bridgelabz/stackandqueue/Stackandqueue.java
+++ stackandqueue/src/main/java/com/bridgelabz/stackandqueue/Stackandqueue.java
@@ -1,41 +1,72 @@
 package com.bridgelabz.stackandqueue;
 
 public class Stackandqueue {
-	Node top;
-	Node next;
+    Node top;
+    Node next;
 
-	public class Node {
-		int data;
-		Node next;
-	}
+    public class Node {
+        int data;
+        Node next;
+    }
 
-	public void Node() {
-		this.top = null;
-	}
+    public void Node() {
+        this.top = null;
+    }
 
-	public void push(int number) {
-		Node node = new Node();
-		node.data = number;
-		node.next = top;
-		top = node;
-	}
+    public void push(int number) {
+        Node node = new Node();
+        node.data = number;
+        node.next = top;
+        top = node;
+    }
 
-	public void printStack() {
-		Node node = top;
-		while (node != null) {
-			System.out.println(node.data);
-			node = node.next;
+    public boolean isEmpty() {
+        return top == null;
+    }
 
-		}
-	}
+    public int peek() {
 
-	public static void main(String[] args) {
-		Stackandqueue stackoperation = new Stackandqueue();
-		stackoperation.push(70);
-		stackoperation.push(30);
-		stackoperation.push(56);
-		stackoperation.printStack();
+        if (!isEmpty()) {
+            return top.data;
+        }
 
-	}
+        else {
+            System.out.println("stack is empty.");
+            return -1;
+        }
+
+    }
+
+    public void pop() {
+        while (top != null) {
+            System.out.println("this is peak now :: " + peek());
+            top = top.next;
+            System.out.println("one item removed from stack...");
+        }
+
+        System.out.println("now stack is empty :: ");
+    }
+
+    public void printStack() {
+        Node node = top;
+
+        while (node != null) {
+            System.out.println(node.data);
+            node = node.next;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Stackandqueue stackoperation = new Stackandqueue();
+
+        stackoperation.push(70);
+        stackoperation.push(30);
+        stackoperation.push(56);
+        stackoperation.printStack();
+        stackoperation.pop();
+        stackoperation.printStack();
+
+    }
 
 }
